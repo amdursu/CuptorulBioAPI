7b179e954fc6ee40798cc8c52825be755a805e1b
Andrei Ursu <amdursu@gmail.com>
2022-10-06 00:09:36 +0300

    vercel test 7

--- src/routes/stocks.ts
+++ src/routes/stocks.ts
@@ -1,22 +1,22 @@
 import express from "express";
 import {
-  deleteStock,
-  getStocks,
-  higherQuantity,
-  insertStock,
-  lowerQuantity,
-  updateStock,
+  // deleteStock,
+  // getStocks,
+  // higherQuantity,
+  // insertStock,
+  // lowerQuantity,
+  // updateStock,
   testVercel,
 } from "../controllers/stocks";
 
 const router = express.Router();
 
 router.get("/testVercel", testVercel);
-router.get("/getStocks", getStocks);
-router.post("/insertStock", insertStock);
-router.put("/updateStock", updateStock);
-router.delete("/deleteStock", deleteStock);
-router.patch("/higherQuantity", higherQuantity);
-router.patch("/lowerQuantity", lowerQuantity);
+// router.get("/getStocks", getStocks);
+// router.post("/insertStock", insertStock);
+// router.put("/updateStock", updateStock);
+// router.delete("/deleteStock", deleteStock);
+// router.patch("/higherQuantity", higherQuantity);
+// router.patch("/lowerQuantity", lowerQuantity);
 
 export default router;
